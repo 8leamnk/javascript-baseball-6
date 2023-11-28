c1dbd68d979223f9b676162e389e2e7980033350
8leamnk <silkviewrk@gmail.com>
2023-11-28 21:35:33 +0900

    feat: 컴퓨터 숫자 선택 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import { Console } from '@woowacourse/mission-utils';
+import { Console, Random } from '@woowacourse/mission-utils';
 
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+
+    const computer = [];
+
+    while (computer.length < 3) {
+      const number = Random.pickNumberInRange(1, 9);
+
+      if (!computer.includes(number)) {
+        computer.push(number);
+      }
+    }
 
     const playerAnswer = await Console.readLineAsync('숫자를 입력해주세요 : ');
   }

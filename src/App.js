bbcf067f9a9dc0dba3bb417f517711051cb42245
8leamnk <silkviewrk@gmail.com>
2023-11-28 21:42:10 +0900

    feat: 입력한 플레이어의 숫자에 대한 유효성 검사 기능을 추가

--- src/App.js
+++ src/App.js
@@ -15,6 +15,32 @@ class App {
     }
 
     const playerAnswer = await Console.readLineAsync('숫자를 입력해주세요 : ');
+    const player = [];
+
+    // 유효성 검사 기능
+    const NOT_NUMBER = /[^0-9]/;
+
+    if (NOT_NUMBER.test(playerAnswer)) {
+      throw new Error('[ERROR] 숫자만 입력하세요.');
+    }
+
+    playerAnswer.forEach((string) => {
+      const number = Number(string);
+
+      if (player.includes(number)) {
+        throw new Error('[ERROR] 중복된 숫자가 있습니다.');
+      }
+
+      if (number < 1 || number > 9) {
+        throw new Error('[ERROR] 1부터 9까지 서로 다른 수를 입력하세요.');
+      }
+
+      player.push(number);
+    });
+
+    if (player.length !== 3) {
+      throw new Error('[ERROR] 3개의 수를 입력하세요.');
+    }
   }
 }
 

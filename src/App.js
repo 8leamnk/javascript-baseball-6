b808eca2f26b5c55ce18f13278d9c7fb7387dd64
8leamnk <silkviewrk@gmail.com>
2023-11-28 21:32:56 +0900

    feat: 플레이어의 숫자 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { Console } from '@woowacourse/mission-utils';
 class App {
   async play() {
     Console.print('숫자 야구 게임을 시작합니다.');
+
+    const playerAnswer = await Console.readLineAsync('숫자를 입력해주세요 : ');
   }
 }
 
